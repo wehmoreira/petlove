95ffc260d4941f3d0c4909727bc703e49d82f064
wehmoreira <wehmoreira@gmail.com>
2019-07-04 11:37:52 -0300

    adicionando mensagens de erro nas views

--- spec/requests/pessoas_spec.rb
+++ spec/requests/pessoas_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-describe "Pessoas", type: :request do
-  describe "GET /pessoas" do
+describe 'Pessoas', type: :request do
+  describe 'GET /pessoas' do
     before { get pessoas_path }
     context 'nenhuma pessoa cadastrada' do
-      it "works!" do
+      it 'responde com status http 200' do
         expect(response).to have_http_status(200)
       end
       it 'retorna uma mensagem' do
