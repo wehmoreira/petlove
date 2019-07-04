287ea70bcb433af72bd0f65d4f70fd5c3856315b
wehmoreira <wehmoreira@gmail.com>
2019-07-04 17:34:49 -0300

    refatorando factory

--- spec/factories/pessoas.rb
+++ spec/factories/pessoas.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :pessoa do
     nome { "Nome Teste da Silva" }
     documento { "%09d" % rand(999999999) }
-    data_nascimento { Time.now.strftime("%Y-%-m-%-d") }
+    data_nascimento { Time.now.strftime("%Y-%m-%d") }
   end
 end
