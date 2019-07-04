fcfa5163b3b10ec2cc0c9fdaf232cc741a7cf8ac
wehmoreira <wehmoreira@gmail.com>
2019-07-04 11:38:47 -0300

    conficuracoes do pry, factory_bot e database cleaner para testes do rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,9 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+# Using pry for debugging
+require 'pry'
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -54,4 +57,19 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Configuring test suite using factory_bot
+  config.include FactoryBot::Syntax::Methods
+
+  # Setting Database Cleaner strategies for RSpec
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
