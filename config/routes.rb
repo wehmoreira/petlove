0c742ca4227f85f737b71d899fa09bf0a6dfbbb3
wehmoreira <wehmoreira@gmail.com>
2019-07-07 19:37:16 -0300

    setando rota principal

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'people#index'
+
   resources :people do
     resources :animals
   end
