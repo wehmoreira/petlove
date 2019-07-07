b2e59cf951dc7924cc20c1813047368dab49b9da
wehmoreira <wehmoreira@gmail.com>
2019-07-07 18:01:31 -0300

    incluindo resource aninhado para animals

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :people
+  resources :people do
+    resources :animals
+  end
 end
